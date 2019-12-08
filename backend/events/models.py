2e7e839ae026c799a91d6e14fe604fc19ffd4f64
Bram <BramOriginal@griet-wl.lan>
2019-12-08 14:32:17 +0100

    Added multiple possible categories to the category field

--- backend/events/models.py
+++ backend/events/models.py
@@ -3,9 +3,24 @@ import datetime
 
 
 class Event(models.Model):
+    CATEGORY_CHOICES = [
+        ("PA", "Party")
+        ("CO", "Concert")
+        ("CF", "Conference")
+        ("MU", "Meetup")
+        ("SE", "Sporting Event")
+        ("FE", "Festival")
+        ("FI", "Film")
+        ("TH", "Theater")
+        ("CS", "Comedy Show")
+        ("AE", "Art Exhibition")
+        ("OT", "Other")
+    ]
+
     name = models.CharField(max_length=255)
     description = models.TextField()
-    category = models.CharField(max_length=30, default="Other")
+    category = models.CharField(
+        max_length=2, choices=CATEGORY_CHOICES, default="OT")
     date = models.DateField(default=datetime.date.today)
     startTime = models.TimeField(default=datetime.time)
     endTime = models.TimeField(default=datetime.time)
